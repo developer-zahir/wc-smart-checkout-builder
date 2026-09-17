c40a5f11df939e0ef09521dc93e7a759bdf3ecdb
developer_zahir <zahirulislam.iv@gmail.com>
2026-09-17 23:27:05 +0600

    Release v1.3.3: Fix missing Group_Control_Background import causing Elementor editor fatal error

--- includes/class-elementor-widget.php
+++ includes/class-elementor-widget.php
@@ -10,6 +10,7 @@ use Elementor\Controls_Manager;
 use Elementor\Group_Control_Typography;
 use Elementor\Group_Control_Border;
 use Elementor\Group_Control_Box_Shadow;
+use Elementor\Group_Control_Background;
 use Elementor\Repeater;
 
 /**
